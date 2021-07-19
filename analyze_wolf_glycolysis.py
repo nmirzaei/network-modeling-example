cdd488e4458127b201677f1923a36eb97a3f8d1d
Lucian Smith <lpsmith@uw.edu>
2021-07-19 07:08:09 -0700

    Get the Antimony string.

--- analyze_wolf_glycolysis.py
+++ analyze_wolf_glycolysis.py
@@ -7,8 +7,13 @@ Created on Mon Jul 19 06:54:30 2021
 
 import tellurium as te
 import roadrunner
+import antimony
 
 r = te.loads("https://www.ebi.ac.uk/biomodels/model/download/BIOMD0000000206.2?filename=BIOMD0000000206_url.xml")
 
 r.simulate(0,1,500)
 r.plot()
+
+sbml = r.getSBML()
+antimony.loadSBMLString(sbml)
+print(antimony.getAntimonyString())
