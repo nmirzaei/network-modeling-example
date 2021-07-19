ddb8b6eb8c793190cd8d91656981a6f84927b643
Lucian Smith <lpsmith@uw.edu>
2021-07-19 07:02:10 -0700

    Initial commit of the model.

---
+++ analyze_wolf_glycolysis.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 19 06:54:30 2021
+
+@author: Lucian
+"""
+
+import tellurium as te
+import roadrunner
+
+r = te.loads("https://www.ebi.ac.uk/biomodels/model/download/BIOMD0000000206.2?filename=BIOMD0000000206_url.xml")
+
+r.simulate()
+r.plot()
