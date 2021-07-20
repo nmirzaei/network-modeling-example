0181c54cdca3dad5588f760ed3734c2b3070cf3b
Lucian Smith <lpsmith@uw.edu>
2021-07-20 12:20:10 -0700

    Fix: nad and nadh were reversed
    Original model just used 'nad' for a single species, which I assumed was 'NAD' (oddly enough).  But this turned out to actually be NADH, meaning the reverse was NAD.

--- analyze_wolf_glycolysis.py
+++ analyze_wolf_glycolysis.py
@@ -15,14 +15,14 @@ model *Wolf2000_Glycolytic_Oscillations()
   // Reactions:
   v1: s1 + 2 atp -> s2 + 2 adp + 2 $pIn; k1*s1*atp/(1 + (atp/ki)^n);
   v2: s2 -> 2 s3; k2*s2;
-  v3: s3 + nad + adp + $pIn -> s4 + atp + nadh; ((k31*k32*s3*nad*adp - k33*k34*s4*atp*nadh)/(k33*nadh + k32*adp));
+  v3: s3 + NADH + adp + $pIn -> s4 + atp + NAD; ((k31*k32*s3*NADH*adp - k33*k34*s4*atp*NAD)/(k33*NAD + k32*adp));
   v4: s4 + adp + $pIn -> s5 + atp; k4*s4*adp;
   v5: s5 -> s6; k5*s5;
   v7: atp -> adp + $pIn; k7*atp;
-  v8: s3 + nadh -> nad; k8*s3*nadh;
+  v8: s3 + NAD -> NADH; k8*s3*NAD;
   v9: s6o -> ; k9*s6o;
   v10: s6 -> 0.1 s6o; k10*(s6 - s6o);
-  v6: s6 + nadh -> nad; k6*s6*nadh;
+  v6: s6 + NAD -> NADH; k6*s6*NAD;
   v0:  -> s1; k0;
 
   // Species initializations:
@@ -32,8 +32,8 @@ model *Wolf2000_Glycolytic_Oscillations()
   pIn = 1000
   s2 = 5;
   s3 = 0.6;
-  nad = 0.6;
-  nadh = ntot - nad
+  NADH = 0.6;
+  NAD = ntot - NADH
   s4 = 0.7;
   s5 = 8;
   s6 = 0.08;
@@ -66,7 +66,7 @@ model *Wolf2000_Glycolytic_Oscillations()
   atp is "ATP";
   s2 is "F16P";
   s3 is "Triose_Gly3Phos_DHAP";
-  nad is "NAD";
+  NADH is "NADH";
   s4 is "3PG";
   s5 is "Pyruvate";
   s6 is "Acetaldehyde";
